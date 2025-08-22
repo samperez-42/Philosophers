c7332e6a16570f3974716dba4f23929605f0c385
samperez <samperez@student.42malaga.com>
2025-08-22 14:45:07 +0200

    Philosopher routine done, need to implement the death and meal count controls

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: samperez <samperez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 15:27:27 by samperez          #+#    #+#             */
-/*   Updated: 2025/08/22 14:06:35 by samperez         ###   ########.fr       */
+/*   Updated: 2025/08/22 14:35:08 by samperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,35 @@ void	*watcher(void *rule)
 	return (r);
 }
 
-// To do:
 // Take fork / Eat / Pass fork  / Sleep / Think / Repeat
-/* void	philos_routine(t_philo *philo)
+void	philos_routine(t_philo *philo)
 {
-	
-} */
+	pthread_mutex_lock(philo->r_fork);
+	philo_msg("has taked a fork", philo);
+	if (philo->r->n_philo == 1)
+	{
+		ft_usleep(philo->r->time_to_die);
+		pthread_mutex_unlock(philo->r_fork);
+		philo_msg("has no one to eat with him, how sad", philo);
+		return ;
+	}
+	pthread_mutex_lock(philo->l_fork);
+	philo_msg("has taken a fork", philo);
+	philo_msg("is eating", philo);
+	ft_usleep(philo->r->time_to_eat);
+	philo->times_eaten++;
+	pthread_mutex_unlock(philo->r_fork);
+	pthread_mutex_unlock(philo->l_fork);
+	philo_msg("is sleeping", philo);
+	ft_usleep(philo->r->time_to_sleep);
+	philo_msg("is thinking", philo);
+}
 
 void	*rtn(void *arg)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
-	if (philo->r->n_philo == 1)
-	{
-		philo_msg("has taken a fork", philo);
-		ft_usleep(philo->r->time_to_die);
-		philo_msg("has no one to eat with him, how sad, he died", philo);
-		return (NULL);
-	}
 	if (philo->id % 2 != 0)
 		ft_usleep(2);
 	/* while (not_dead() == EXIT_SUCCESS)
